30988f2f6509360dee7eeffe8685afb3b9cf9eae
MohammedBadry <mahamedbadry2003@gmail.com>
2023-09-02 22:00:33 +0300

    Add the table responsive feature for tasks table

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -12,41 +12,42 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                 
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Start Date</th>
-                                <th scope="col">End Date</th>
-                                <th scope="col">Date Created</th>
-                                <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($tasks as $task)
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
                                 <tr>
-                                    <th scope="row">{{$task->id}}</th>
-                                    <td>{{$task->name}}</td>
-                                    <td>{{$task->start_date}}</td>
-                                    <td>{{$task->end_date}}</td>
-                                    <td>{{$task->created_at}}</td>
-                                    <td>
-                                        <a href="{{ route('tasks.edit', $task->id)}}" class="text-primary">Edit</a>
-
-                                        <form method="post" action="{{route('tasks.destroy', $task->id)}}" 
-                                            style="float: right;"
-                                            onSubmit="alert('Are you sure you wish to delete?');">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="text-danger">Delete</button>
-                                        </form>
-                                    </td>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Start Date</th>
+                                    <th scope="col">End Date</th>
+                                    <th scope="col">Date Created</th>
+                                    <th scope="col">Actions</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                
+                            </thead>
+                            <tbody>
+                                @foreach($tasks as $task)
+                                    <tr>
+                                        <th scope="row">{{$task->id}}</th>
+                                        <td>{{$task->name}}</td>
+                                        <td>{{$task->start_date}}</td>
+                                        <td>{{$task->end_date}}</td>
+                                        <td>{{$task->created_at}}</td>
+                                        <td>
+                                            <a href="{{ route('tasks.edit', $task->id)}}" class="text-primary">Edit</a>
+
+                                            <form method="post" action="{{route('tasks.destroy', $task->id)}}" 
+                                                style="float: right;"
+                                                onSubmit="alert('Are you sure you wish to delete?');">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="text-danger">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>                
                 </div>
             </div>
         </div>
